037b69a18ebac61fd6dc03c8b729c9e0a1ec3ce8
boruch d <dubywider@gmail.com>
2019-05-16 20:08:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc(array)
+array.sort
 
+ end
+
+def sort_array_desc(array)  
+
+array.sort.reverse 
+
+end
+
+def sort_array_char_count(array)
+  
+  array.sort_by {|x| x.length}
+  
+end
+def swap_elements(array)
+  
+ array[0], array[1], array[2] =array[0], array[2], array[1]
+  
+end
+
+def reverse_array (array)
+  array.reverse
+  
+end
+def kesha_maker(array)
