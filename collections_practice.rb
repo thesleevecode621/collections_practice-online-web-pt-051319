094ac965badc4c063017aee8d3e9b3f816b7939e
boruch d <dubywider@gmail.com>
2019-05-16 20:16:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,9 +24,9 @@ def reverse_array (array)
   array.reverse
   
 end
-def kesha_maker(array)
+def kesha_maker(name)
   h = []
-  array.each do |name| name[2] = "$"
+  name.each do |name| name[2] = "$"
    name 
  
 end 
