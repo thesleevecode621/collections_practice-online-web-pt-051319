c3b650c3b68baceabc9b069b37c9c7c9837ee81b
boruch d <dubywider@gmail.com>
2019-05-16 20:41:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+array.sort
 
+ end
+
+def sort_array_desc(array)  
+
+array.sort.reverse 
+
+end
+
+def sort_array_char_count(array)
+  
+  array.sort_by {|x| x.length}
+  
+end
+def swap_elements(array)
+  
+ array[0], array[1], array[2] =array[0], array[2], array[1]
+  
+end
+
+def reverse_array (array)
+  array.reverse
+  
+end
+def kesha_maker(array)
+  
+  array.map do |name| name[2] = "$"
+ end 
+  array
+ 
+end 
+ def find_a(array)
+   array.select {|x|x.start_with?("a")}
+   
+ end
+def  sum_array(array)
+  array.inject(:+)
+  
+end
+def add_s(array)
+  array.each_with_index{|x,i| x << "s" unless i == 1}
+end
